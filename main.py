f33ec79336ffaaf2829de07ccad17cc2959fa05d
ssymhoven <simon.symhoven@donner-reuschel.lu>
2024-05-16 14:35:28 +0200

    update

--- main.py
+++ main.py
@@ -1,13 +1,54 @@
-from utility import get_positions, style_positions_with_bars, get_us_sector_data, get_eu_sector_data
+from utility import get_positions, style_positions_with_bars, get_us_sector_data, get_eu_sector_data, \
+    style_index_with_bars, calc_rel_performance, calc_sector_diff, write_mail
+
+threshold = -8
+
+mail_data = {
+    'files': list(),
+    'positions': {},
+}
+mail = True
+
 
 if __name__ == '__main__':
     positions = get_positions()
     us_sector = get_us_sector_data()
     eu_sector = get_eu_sector_data()
+    diff = calc_sector_diff(us=us_sector, eu=eu_sector)
+
+    positions = calc_rel_performance(positions=positions, eu=eu_sector, us=us_sector)
 
     unique_names = positions.index.get_level_values(0).unique()
+
+    files = mail_data.get('files')
+    p = mail_data.get('positions')
 
     for name in unique_names:
         subset = positions.loc[name]
         details_chart = style_positions_with_bars(positions=subset, name=name)
+        files.append(details_chart)
 
+        filtered_subset = subset[
+            (subset['1D vs. Sector'] < threshold) |
+            (subset['5D vs. Sector'] < threshold) |
+            (subset['1MO vs. Sector'] < threshold) |
+            (subset['YTD vs. Sector'] < threshold)
+        ]
+
+        underperformed_details_chart = style_positions_with_bars(positions=filtered_subset, name=f'{name}_underperformed')
+        p.update({name: underperformed_details_chart})
+
+    us_sector_chart = style_index_with_bars(index=us_sector, name='US')
+    eu_sector_chart = style_index_with_bars(index=eu_sector, name='EU')
+    diff_sector_chart = style_index_with_bars(index=diff, name='EU_vs_US')
+
+    mail_data.update({
+        'sector': {
+            'SXXP Index': eu_sector_chart,
+            'SPX Index': us_sector_chart,
+            'SXXP Index vs. SPX Index': diff_sector_chart
+        }
+    })
+
+    if mail:
+        write_mail(data=mail_data)
--- utility.py
+++ utility.py
@@ -1,13 +1,12 @@
 import os
+from datetime import datetime
+from typing import Dict
 
-import imgkit
 import pandas as pd
-from matplotlib import pyplot as plt
+import win32com.client as win32
 from matplotlib.colors import LinearSegmentedColormap
 from source_engine.opus_source import OpusSource
-import seaborn as sns
 import dataframe_image as dfi
-from IPython.display import display
 
 output_dir = "output"
 os.makedirs(os.path.join(output_dir, "images"), exist_ok=True)
@@ -27,7 +26,7 @@ query = f"""
         positions.isin as ISIN,
         positions.bloomberg_query as Query,
         positions.average_entry_quote as AEQ,
-        positions.currency as Currency,
+        positions.currency as Crncy,
         positions.volume as Volume,
         positions.gics_industry_sector as Sector,
         positions.dr_class_level_2 as 'Region'
@@ -55,17 +54,44 @@ opus = OpusSource()
 def get_positions() -> pd.DataFrame:
     df = opus.read_sql(query=query)
     stocks = get_stocks_data()
-    positions = pd.merge(df, stocks[['bloomberg_query', 'Last Price', '5D', '1MO', 'YTD']],
+
+    def combine_currencies(row):
+        if row['Crncy'] == row['Currency']:
+            return row['Crncy']
+        else:
+            return f"{row['Crncy']}/{row['Currency']}"
+
+    positions = pd.merge(df, stocks[['bloomberg_query', 'Currency', 'Last Price', '1D', '5D', '1MO', 'YTD']],
                          left_on='Query', right_on='bloomberg_query',
                          how='left')
+    positions['Currency'] = positions.apply(combine_currencies, axis=1)
+    positions.drop(columns=['Crncy'], inplace=True)
+
     positions.set_index(['Name', 'Position Name'], inplace=True)
     positions['% since AEQ'] = pd.to_numeric(((positions['Last Price'] - positions['AEQ']) / positions['AEQ']) * 100, errors='coerce')
     return positions
 
 
+def calc_rel_performance(positions: pd.DataFrame, us: pd.DataFrame, eu: pd.DataFrame) -> pd.DataFrame:
+
+    def calculate_difference(row, benchmark_df):
+        benchmark_row = benchmark_df.loc[row['Sector']]
+
+        for time_frame in ['1D', '5D', '1MO', 'YTD']:
+            row[f'{time_frame} vs. Sector'] = row[time_frame] - benchmark_row[time_frame]
+
+        return row
+
+    positions = positions.apply(
+        lambda row: calculate_difference(row, eu if row['Region'] == 'EU' else us), axis=1
+    )
+
+    return positions
+
+
 def get_stocks_data() -> pd.DataFrame:
     df = pd.read_excel('single-stocks.xlsx', sheet_name='Stocks', header=0)
-    df = df.rename(columns={'CURRENT_TRR_5D': '5D', 'CURRENT_TRR_1MO': '1MO', 'CURRENT_TRR_YTD': 'YTD'})
+    df = df.rename(columns={'CURRENT_TRR_1D': '1D', 'CURRENT_TRR_5D': '5D', 'CURRENT_TRR_1MO': '1MO', 'CURRENT_TRR_YTD': 'YTD'})
     mask = df['Currency'] == 'GBp'
     df.loc[mask, 'Currency'] = 'GBP'
     df.loc[mask, 'Last Price'] /= 100
@@ -73,17 +99,21 @@ def get_stocks_data() -> pd.DataFrame:
 
 
 def get_us_sector_data() -> pd.DataFrame:
-    return pd.read_excel('single-stocks.xlsx', sheet_name='US Sector', header=0, index_col=0)
+    df = pd.read_excel('single-stocks.xlsx', sheet_name='US Sector', header=0, index_col=0)
+    df.drop('Query', inplace=True, axis=1)
+    df = df.rename(columns={'CURRENT_TRR_1D': '1D', 'CURRENT_TRR_5D': '5D', 'CURRENT_TRR_1MO': '1MO', 'CURRENT_TRR_YTD': 'YTD'})
+    return df
 
 
 def get_eu_sector_data() -> pd.DataFrame:
     df = pd.read_excel('single-stocks.xlsx', sheet_name='EU Sector', header=0)
     df.drop('Query', inplace=True, axis=1)
-    df = df.rename(columns={'CURRENT_TRR_5D': '5D', 'CURRENT_TRR_1MO': '1MO', 'CURRENT_TRR_YTD': 'YTD'})
+    df = df.rename(columns={'CURRENT_TRR_1D': '1D', 'CURRENT_TRR_5D': '5D', 'CURRENT_TRR_1MO': '1MO', 'CURRENT_TRR_YTD': 'YTD'})
 
     def calculate_weighted_trrs(group):
         weight = group['CUR_MKT_CAP'] / group['CUR_MKT_CAP'].sum()
-        d = {'5D': (group['5D'] * weight).sum(),
+        d = {'1D': (group['1D'] * weight).sum(),
+             '5D': (group['5D'] * weight).sum(),
              '1MO': (group['1MO'] * weight).sum(),
              'YTD': (group['YTD'] * weight).sum()}
         return pd.Series(d)
@@ -92,36 +122,188 @@ def get_eu_sector_data() -> pd.DataFrame:
     return df
 
 
+def calc_sector_diff(us: pd.DataFrame, eu: pd.DataFrame) -> pd.DataFrame:
+    common_columns = us.columns.intersection(eu.columns)
+    common_index = us.index.intersection(eu.index)
+
+    diff = pd.DataFrame(index=common_index, columns=common_columns)
+
+    for col in common_columns:
+        for idx in common_index:
+            diff.at[idx, col] = eu.at[idx, col] - us.at[idx, col]
+
+    last_row_diff = eu.loc[eu.index[-1]] - us.loc[us.index[-1]]
+    name = f"{eu.index[-1]} - {us.index[-1]}"
+    diff = pd.concat([diff, pd.DataFrame([last_row_diff.values],
+                                         columns=common_columns,
+                                         index=[name])])
+    diff.index.name = 'GICS'
+    return diff
+
+
 def style_positions_with_bars(positions: pd.DataFrame, name: str) -> str:
-    columns_to_show = ['Position Name', 'AEQ', 'Currency', 'Volume', 'Last Price', '% since AEQ', '5D', '1MO', 'YTD']
+    columns_to_show = ['Position Name', 'AEQ', 'Currency', 'Volume', 'Last Price', '% since AEQ', '1D', '5D', '1MO', 'YTD',
+                       '1D vs. Sector', '5D vs. Sector', '1MO vs. Sector', 'YTD vs. Sector']
     positions = positions.copy().reset_index()[columns_to_show]
 
     aeq_max_abs_value = max(abs(positions['% since AEQ'].min().min()),
                             abs(positions['% since AEQ'].max().max()))
+    trr1d_max_abs_value = max(abs(positions['1D'].min().min()),
+                              abs(positions['1D'].max().max()))
     trr5d_max_abs_value = max(abs(positions['5D'].min().min()),
                               abs(positions['5D'].max().max()))
     trr1mo_max_abs_value = max(abs(positions['1MO'].min().min()),
-                              abs(positions['1MO'].max().max()))
+                               abs(positions['1MO'].max().max()))
     trr_ytd_max_abs_value = max(abs(positions['YTD'].min().min()),
-                              abs(positions['YTD'].max().max()))
+                                abs(positions['YTD'].max().max()))
 
-    positions = positions.sort_values(by='% since AEQ', ascending=False)
+    rel_trr1d_max_abs_value = max(abs(positions['1D vs. Sector'].min().min()),
+                                  abs(positions['1D vs. Sector'].max().max()))
+    rel_trr5d_max_abs_value = max(abs(positions['5D vs. Sector'].min().min()),
+                                abs(positions['5D vs. Sector'].max().max()))
+    rel_trr_trr1mo_max_abs_value = max(abs(positions['1MO vs. Sector'].min().min()),
+                                abs(positions['1MO vs. Sector'].max().max()))
+    rel_trr_ytd_max_abs_value = max(abs(positions['YTD vs. Sector'].min().min()),
+                                abs(positions['YTD vs. Sector'].max().max()))
+
+    positions.sort_values('% since AEQ', ascending=False, inplace=True)
+    positions.set_index('Position Name', inplace=True)
+
     cm = LinearSegmentedColormap.from_list("custom_red_green", ["red", "white", "green"], N=len(positions))
 
     styled = (positions.style.bar(subset='% since AEQ', cmap=cm, align=0, vmax=aeq_max_abs_value, vmin=-aeq_max_abs_value)
+              .bar(subset='1D', cmap=cm, align=0, vmax=trr1d_max_abs_value, vmin=-trr1d_max_abs_value)
               .bar(subset='5D', cmap=cm, align=0, vmax=trr5d_max_abs_value, vmin=-trr5d_max_abs_value)
               .bar(subset='1MO', cmap=cm, align=0, vmax=trr1mo_max_abs_value, vmin=-trr1mo_max_abs_value)
               .bar(subset='YTD', cmap=cm, align=0, vmax=trr_ytd_max_abs_value, vmin=-trr_ytd_max_abs_value)
+              .bar(subset='1D vs. Sector', cmap=cm, align=0, vmax=rel_trr1d_max_abs_value,
+                   vmin=-rel_trr1d_max_abs_value)
+              .bar(subset='5D vs. Sector', cmap=cm, align=0, vmax=rel_trr5d_max_abs_value, vmin=-rel_trr5d_max_abs_value)
+              .bar(subset='1MO vs. Sector', cmap=cm, align=0, vmax=rel_trr_trr1mo_max_abs_value, vmin=-rel_trr_trr1mo_max_abs_value)
+              .bar(subset='YTD vs. Sector', cmap=cm, align=0, vmax=rel_trr_ytd_max_abs_value, vmin=-rel_trr_ytd_max_abs_value)
+              .set_table_styles([
+                    {'selector': 'th.col0',
+                     'props': [('border-left', '1px solid black')]},
+                    {'selector': 'td.col0',
+                     'props': [('border-left', '1px solid black')]},
+                    {'selector': 'th.col5',
+                     'props': [('border-left', '1px solid black')]},
+                    {'selector': 'td.col5',
+                     'props': [('border-left', '1px solid black')]},
+                    {'selector': 'th.col9',
+                     'props': [('border-left', '1px solid black')]},
+                    {'selector': 'td.col9',
+                     'props': [('border-left', '1px solid black')]},
+                    {
+                        'selector': 'th.index_name',
+                        'props': [('min-width', '250px'), ('white-space', 'nowrap')]
+                    }
+              ])
               .format({
                     'AEQ': "{:,.2f}",
                     'Last Price': "{:,.2f}",
                     'Volume': "{:,.0f}",
                     '% since AEQ': "{:.2f}%",
+                    '1D': "{:.2f}%",
                     '5D': "{:.2f}%",
                     '1MO': "{:.2f}%",
                     'YTD': "{:.2f}%",
-              }).hide(axis="index"))
+                    '1D vs. Sector': "{:.2f}%",
+                    '5D vs. Sector': "{:.2f}%",
+                    '1MO vs. Sector': "{:.2f}%",
+                    'YTD vs. Sector': "{:.2f}%",
+              }))
 
-    output_path = f"output/images/{name}_Details.png"
+    output_path = f"output/images/{name.replace(' ', '_')}_Details.png"
     dfi.export(styled, output_path, table_conversion="selenium")
     return output_path
+
+
+def style_index_with_bars(index: pd.DataFrame, name: str) -> str:
+    for col in ['1D', '5D', '1MO', 'YTD']:
+        index[col] = pd.to_numeric(index[col], errors='coerce')
+
+    trr1d_max_abs_value = index['1D'].abs().max()
+    trr5d_max_abs_value = index['5D'].abs().max()
+    trr1mo_max_abs_value = index['1MO'].abs().max()
+    trr_ytd_max_abs_value = index['YTD'].abs().max()
+
+    cm = LinearSegmentedColormap.from_list("custom_red_green", ["red", "white", "green"], N=len(index))
+    styled = (
+        index.style.bar(subset='1D', cmap=cm, align=0, vmax=trr1d_max_abs_value, vmin=-trr1d_max_abs_value)
+        .bar(subset='5D', cmap=cm, align=0, vmax=trr5d_max_abs_value, vmin=-trr5d_max_abs_value)
+        .bar(subset='1MO', cmap=cm, align=0, vmax=trr1mo_max_abs_value, vmin=-trr1mo_max_abs_value)
+        .bar(subset='YTD', cmap=cm, align=0, vmax=trr_ytd_max_abs_value, vmin=-trr_ytd_max_abs_value)
+        .set_table_styles([
+            {'selector': 'th.col0',
+             'props': [('border-left', '1px solid black')]},
+            {'selector': 'td.col0',
+             'props': [('border-left', '1px solid black')]},
+            {'selector': 'tr:last-child th, tr:last-child td',
+             'props': [('border-top', '1px solid black')]}
+        ])
+        .format({
+            '1D': "{:,.2f}",
+            '5D': "{:,.2f}",
+            '1MO': "{:,.2f}",
+            'YTD': "{:,.2f}"
+        }))
+
+    output_path = f"output/images/{name.replace(' ', '_')}_Details.png"
+    dfi.export(styled, output_path, table_conversion="selenium")
+    return output_path
+
+
+def write_mail(data: Dict):
+    outlook = win32.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
+
+    mail.Subject = "Daily Reporting - Risikomanagement"
+    mail.Recipients.Add("pm-aktien")
+    mail.Recipients.Add("sadettin.yildiz@donner-reuschel.de").Type = 2
+
+    def inplace_chart(image_path: str):
+        image_path = os.path.abspath(image_path)
+        attachment = mail.Attachments.Add(Source=image_path)
+        cid = os.path.basename(image_path)
+        attachment.PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001F", cid)
+        return cid
+
+    sector_images_html = ''
+    for key, image_path in data.get('sector', {}).items():
+        cid = inplace_chart(image_path)
+        sector_images_html += f'<p><b>{key}</b><br><img src="cid:{cid}"><br></p>'
+
+    position_images_html = ''
+    for key, image_path in data.get('positions', {}).items():
+        cid = inplace_chart(image_path)
+        position_images_html += f'<p><b>{key}</b><br><img src="cid:{cid}"><br></p>'
+
+    mail.HTMLBody = f"""
+        <html>
+          <head></head>
+          <body>
+            <p>Hi zusammen, <br><br>
+                die Kurse der anhängenden Charts sind vom <b>{datetime.now().strftime('%d.%m.%Y %H:%M')}</b>.<br><br>
+                Hier sind absoluten Entwicklungen der <b>Sektoren</b> für den <b>Euro Stoxx 600</b> und den <b>S&P 500</b>, 
+                sowie die Out/Underperformance des <b>Euro Stoxx 600</b> gegenüber dem <b>S&P 500</b>:<br><br>
+                {sector_images_html}
+                <br><br>
+                Hier sind die <b>Positionen</b> die sich in einem der jeweiligen Betrachtungszeiträume 
+                <b>mindestens 8% schlechter</b> gegebenüber dem jeweiligen Sektor entwickelt haben:<br><br>
+                {position_images_html}
+                <br><br>
+                Im Anhang findet Ihr für alle Fonds eine Übersicht über alle enthaltenen Positionen.
+                <br><br>
+                Liebe Grüße
+            </p>
+          </body>
+        </html>
+    """
+
+    for file_path in data.get('files', []):
+        if os.path.exists(file_path):
+            mail.Attachments.Add(Source=os.path.abspath(file_path))
+
+    mail.Recipients.ResolveAll()
+    mail.Display(True)
